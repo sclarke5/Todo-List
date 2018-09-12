9dae56b62eb1722fe64e9efe1e78c1571d960640
sclarke5 <sclarke5555@gmail.com>
2018-09-12 18:01:58 -0400

    adds basic styling and keypress events" git push

--- v3.js
+++ v3.js
@@ -36,28 +36,61 @@ var todoList = {
     var totalTodos = this.todos.length;
     var completedTodos = 0;
 
-    //get number of compelted todos
-    for (var i = 0; i < totalTodos; i++){
-      if (this.todos[i].completed === true){
+    //get number of compeleted todos
+
+    //OLDER VERSION -- ORIGINAL FOR LOOPS
+
+    // for (var i = 0; i < totalTodos; i++){
+    //   if (this.todos[i].completed === true){
+    //     completedTodos++;
+    //   }
+    // }
+
+    this.todos.forEach(function(todo){
+      if(todo.completed === true){
         completedTodos++;
       }
-    }
+    });
+
     //Case 1: Everything true, make everything false
-    if (completedTodos === totalTodos){
-      //make false
-      for (var i = 0; i < totalTodos; i++){
-        this.todos[i].completed = false;
+
+    // if (completedTodos === totalTodos){
+    //   //OLDER VERSION -- FORLOOPS
+    //   // for (var i = 0; i < totalTodos; i++){
+    //   //   this.todos[i].completed = false;
+    //   // }
+    //  // INTERMEDIATE VERSION--MESSY
+    // this.todos.forEach(function(todo){
+    //   todo.completed = false;
+    // })
+
+    // //Case 2: Otherwise, make everything true
+    // } else {
+    //   //OLDER--FORLOOPS
+    //   // for (var i =0; i < totalTodos; i++){
+    //   //   this.todos[i].completed = true;
+    //   // }
+    //  // INTERMEDIATE VERSION--MESSY
+    //   this.todos.forEach(function(todo) {
+    //     todo.completed = true;
+    //   });
+    // }
+
+    //FINAL, CLEANEST VERSION--FOR EACH OUTSIDE IF/ELSE CONDITIONS
+
+    this.todos.forEach(function(todo){
+      if(completedTodos === totalTodos){
+        todo.completed = false;
+      } else {
+        todo.completed = true;
       }
-    //Case 2: Otherwise, make everything true
-    } else {
-      for (var i =0; i < totalTodos; i++){
-        this.todos[i].completed = true;
-      }
-    }
+    })
+
   }
 };
 
   //Older version; more explicit before refactoring
+
     //Access to display todos button
 // var displayTodosButton = document.getElementById('displayTodosButton');
 // var toggleAllButton = document.getElementById('toggleAllButton');
@@ -105,22 +138,40 @@ var view = {
   displayTodos: function(){
     var todosUl = document.querySelector('ul');
     todosUl.innerHTML = '';
-    for(var i = 0; i < todoList.todos.length; i++){
+
+    // OLDER VERSION--FOR LOOP REPLACED BY FOREACH
+
+    // for(var i = 0; i < todoList.todos.length; i++){
+    //   var todoLi = document.createElement('li');
+    //   var todo = todoList.todos[i];
+    //   var todoTextWithCompletion = '';
+    //
+    //   if(todo.completed === true){
+    //       todoTextWithCompletion = '(X) ' + todo.todoText;
+    //   }else{
+    //       todoTextWithCompletion = '( ) ' + todo.todoText;
+    //   }
+    //
+    //   todoLi.id = i;
+    //   todoLi.textContent = todoTextWithCompletion;
+    //   todoLi.appendChild(this.createDeleteButton());
+    //   todosUl.appendChild(todoLi);
+    // }
+    todoList.todos.forEach(function(todo, position){
       var todoLi = document.createElement('li');
-      var todo = todoList.todos[i];
       var todoTextWithCompletion = '';
 
-      if(todo.completed === true){
-          todoTextWithCompletion = '(X) ' + todo.todoText;
-      }else{
-          todoTextWithCompletion = '( ) ' + todo.todoText;
-      }
+        if(todo.completed === true){
+            todoTextWithCompletion = '(X) ' + todo.todoText;
+        }else{
+            todoTextWithCompletion = '( ) ' + todo.todoText;
+        }
 
-      todoLi.id = i;
+      todoLi.id = position;
       todoLi.textContent = todoTextWithCompletion;
-      todoLi.appendChild(this.createDeleteButton());
+      todoLi.appendChild(this.createDeleteButton()); //will not work--this no longer refers to view object while inside callback
       todosUl.appendChild(todoLi);
-    }
+    }, this); //fixes above problem using forEach's optional 'this' argument
   },
   createDeleteButton: function(){
     var deleteButton = document.createElement('button');
@@ -138,6 +189,18 @@ var view = {
       //check if element clicked is delete button
       if(elementClicked.className === 'deleteButton'){
         handlers.deleteTodo(parseInt(elementClicked.parentNode.id));
+      }
+    });
+    var addInput = document.getElementById("addTodoTextInput");
+    addInput.addEventListener('keypress', function(keyPressed){
+      if(keyPressed.keyCode === 13){
+        handlers.addTodo();
+      }
+    });
+    var changeInput = document.getElementById("changeTodoTextInput");
+    changeInput.addEventListener('keypress', function(keyPressed){
+      if(keyPressed.keyCode === 13){
+        handlers.changeTodo();
       }
     });
   }
